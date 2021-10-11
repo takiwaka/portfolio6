e3187ff34328f7fc466f8f543f5c3c6e5ba3e9cf
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-10-11 18:02:34 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
 
 function App() {
   return (
     <div className="App">
+      <Todo
+        todos={['foo', 'bar']}
+        onClickAddButton={(todo: string): void => {
+          console.log(todo);
+        }}
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
