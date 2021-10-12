3480e2570ea436396e9c4588949ecc7e92ac808f
kawakita <hakusou.kokuyou@gmail.com>
2021-10-13 06:55:26 +0900

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import "./App.css";
-import Todo from "./Components/Parts/Todo";
-import TodoRedux from "./Components/Parts/TodoRedux";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <Todo
-        onClickAddButton={(todo: string): void => {
-          console.log(todo);
-        }}
-      />
-      <TodoRedux
-        todos={["foo", "bar"]}
-        onClickAddButton={(todo: string): void => {
-          console.log(todo);
-        }}
-      />
-    </div>
-  );
-}
+
+    return (
+      <div>
+
+      </div>
+    );
+  }
 
 export default App;
